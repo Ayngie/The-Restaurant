4cad56ea2f1b584cc1368ed660b701884ce6c026
Ayngie <angelicareutersward@gmail.com>
2023-06-01 14:43:23 +0200

    ✨ (feat): added Link to StyledButton

--- fed22s-therestaurant/src/components/pages/Home.tsx
+++ fed22s-therestaurant/src/components/pages/Home.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import { NormalButton } from "../styled/StyledButtons";
 export const Home = () => {
   return (
     <>
       <h1>Välkommen till Restaurangen!</h1>
-      <NormalButton>Gå till bokningar</NormalButton>
+      <Link to={"/booking"}>
+        <NormalButton>Gå till bokningar</NormalButton>
+      </Link>
     </>
   );
 };
