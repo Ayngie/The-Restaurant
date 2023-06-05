a496a0a26f66c204435cd28f70cf21603f789493
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-05 22:46:37 +0200

    (feat): import ShowCalendar

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -1,9 +1,11 @@
 import { ChooseBooking } from "../ChooseBooking";
+import { ShowCalendar } from "../ShowCalendar";
 
 export const Booking = () => {
   return (
     <>
       <ChooseBooking></ChooseBooking>
+      <ShowCalendar></ShowCalendar>
     </>
   );
 };
