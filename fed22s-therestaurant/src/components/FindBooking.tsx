7b3dde6ecd711960aa46629bd1c42657067bcac1
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-07 10:28:28 +0200

    (feat): create userinput for number of guests

--- fed22s-therestaurant/src/components/FindBooking.tsx
+++ fed22s-therestaurant/src/components/FindBooking.tsx
@@ -1,3 +1,32 @@
+import { NormalButton } from "./styled/StyledButtons";
+import { StyledInput } from "./styled/StyledInput";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { ColumnWrapper } from "./styled/Wrappers";
+
 export const FindBooking = () => {
-  return <>FindBooking!!!</>;
+  const [handleInput, sethandleInput] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState("");
+
+  const handleClick = (e: FormEvent) => {
+    e.preventDefault();
+    setNumberOfGuests(handleInput);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    sethandleInput(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleClick}>
+      <ColumnWrapper>
+        <StyledInput
+          type="number"
+          placeholder="Antal personer"
+          value={numberOfGuests}
+          onChange={handleChange}
+        ></StyledInput>
+        <NormalButton>Sök</NormalButton>
+      </ColumnWrapper>
+    </form>
+  );
 };
