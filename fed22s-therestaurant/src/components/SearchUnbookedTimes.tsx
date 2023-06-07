a3f4c4c9340fd3375591e4ef11bcc69b91530c36
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-07 16:41:02 +0200

    (feat): add get bookings by date

--- fed22s-therestaurant/src/components/SearchUnbookedTimes.tsx
+++ fed22s-therestaurant/src/components/SearchUnbookedTimes.tsx
@@ -1,28 +1,45 @@
 import { useState } from "react";
 import { NumberOfGuests } from "./NumberOfGuests";
 import { ShowCalendar } from "./ShowCalendar";
-import { CalendarWrapper, ColumnWrapper, RowWrapper } from "./styled/Wrappers";
+import { CalendarWrapper, RowWrapper } from "./styled/Wrappers";
 import Dropdown, { Option } from "react-dropdown";
 import "react-dropdown/style.css";
+import { NormalButton } from "./styled/StyledButtons";
+import { getBookingsByDate } from "../services/bookingService";
 
 export const SearchUnbookedTimes = () => {
   const [numberOfGuests, setNumberOfGuests] = useState("");
   const getNumberOfGuests = (value: string) => {
     setNumberOfGuests(value);
   };
-  console.log(numberOfGuests);
 
   const [clickedDate, setClickedDate] = useState("");
   const getDate = (value: string) => {
     setClickedDate(value);
   };
-  console.log(clickedDate);
 
   const options = ["18:00", "22:00"];
   const defaultOption = options[0];
 
+  const [bookingInfo, setBookingInfo] = useState({
+    guests: "",
+    date: "",
+    time: "",
+  });
+
+  const [clickedTime, setClickedTime] = useState(defaultOption);
   const handleChange = (option: Option) => {
-    console.log("handleChange", option.value);
+    setClickedTime(option.value);
+  };
+  console.log("setBookinginfo", bookingInfo);
+
+  const handleSearch = () => {
+    setBookingInfo({
+      guests: numberOfGuests,
+      date: clickedDate,
+      time: clickedTime,
+    });
+    getBookingsByDate(bookingInfo.date);
   };
 
   return (
@@ -31,18 +48,17 @@ export const SearchUnbookedTimes = () => {
         <CalendarWrapper>
           <ShowCalendar getDate={getDate}></ShowCalendar>
         </CalendarWrapper>
-        <ColumnWrapper>
-          <Dropdown
-            options={options}
-            onChange={handleChange}
-            value={defaultOption}
-            placeholder="Välj tid"
-          />
-          <NumberOfGuests
-            getNumberOfGuests={getNumberOfGuests}
-          ></NumberOfGuests>
-        </ColumnWrapper>
+
+        <NumberOfGuests getNumberOfGuests={getNumberOfGuests}></NumberOfGuests>
+        <label>Sittning:</label>
+        <Dropdown
+          options={options}
+          onChange={handleChange}
+          value={defaultOption}
+          placeholder="Välj tid"
+        />
       </RowWrapper>
+      <NormalButton onClick={handleSearch}>Sök</NormalButton>
     </>
   );
 };
