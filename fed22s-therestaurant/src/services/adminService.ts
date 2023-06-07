91c3d4eaa639a7513395c03603908aba49d9cbb7
Per Berge <per.berge93@gmail.com>
2023-06-07 21:33:12 +0200

    (feat): admin-api-routes to update a booking

--- fed22s-therestaurant/src/services/adminService.ts
+++ fed22s-therestaurant/src/services/adminService.ts
@@ -15,3 +15,22 @@ export const getBookingsByDate = async (date: string): Promise<IBooking[]> => {
 export const getBookingById = async (id: string): Promise<IBooking> => {
   return await get<IBooking>(`/${id}`);
 };
+
+export const updateBooking = async (updateBooking: IBooking, id: string) => {
+  const response = await axios.put(`${BASE_URL}/${id}`, {
+    numberOfGuests: updateBooking.numberOfGuests,
+    date: updateBooking.date,
+    time: updateBooking.time,
+    guest: {
+      name: updateBooking.guest.name,
+      email: updateBooking.guest.email,
+      phoneNumber: updateBooking.guest.phoneNumber,
+    },
+  });
+  return response.data;
+};
+/*
+Exempel på hur man kan uppdatera användare.
+await updateBooking(updated, "6480daa0eb1a360858e6b2f9");
+måste skicka in hela boknings samt id:et till bokning
+*/
