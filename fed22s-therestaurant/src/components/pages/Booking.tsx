fe9de6a22f2718beaaf74fbb267210e3035e8a59
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-07 16:39:04 +0200

    (refactor): removed unnecesary code

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -1,16 +1,11 @@
 import { useState } from "react";
 import { ChooseBooking } from "../ChooseBooking";
 import { CompleteBooking } from "../CompleteBooking";
-import { FindBooking } from "../FindBooking";
-import { ShowCalendar } from "../ShowCalendar";
-import { CalendarWrapper, ColumnWrapper } from "../styled/Wrappers";
-import { NumberOfGuests } from "../NumberOfGuests";
+import { ColumnWrapper } from "../styled/Wrappers";
 import { SearchUnbookedTimes } from "../SearchUnbookedTimes";
 
 export const Booking = () => {
   const timeToFillOutForm: boolean = true; //satt på true nu för att kunna visa komponent tills vidare...
-  // Päronet
-  // härifrån kör vi lifting state upp till alla syskon.
   const [clickedNewBooking, setClickedNewBooking] = useState(false);
   const createNewBooking = (value: boolean) => {
     setClickedNewBooking(value);
