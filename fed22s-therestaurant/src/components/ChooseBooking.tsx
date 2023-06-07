3af2635ade746a2f4ba6a5096759caa2f800d557
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-07 10:45:46 +0200

    (feat): implement lifting state up to add functionality to button Ny bokning

--- fed22s-therestaurant/src/components/ChooseBooking.tsx
+++ fed22s-therestaurant/src/components/ChooseBooking.tsx
@@ -1,11 +1,19 @@
 import { NormalButton } from "./styled/StyledButtons";
+import { ColumnWrapper, RowWrapper } from "./styled/Wrappers";
 
+interface ICreateNewBooking {
+  createNewBooking(userInput: boolean): void;
+}
 
+export const ChooseBooking = ({ createNewBooking }: ICreateNewBooking) => {
+  const handleClick = () => {
+    createNewBooking(true);
+  };
   return (
     <>
       <ColumnWrapper>
         <RowWrapper>
-          <NormalButton onClick={showCalendar}>Ny bokning</NormalButton>
+          <NormalButton onClick={handleClick}>Ny bokning</NormalButton>
           <NormalButton>Sök din bokning</NormalButton>
         </RowWrapper>
       </ColumnWrapper>
