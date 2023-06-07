85203588072bac2a3ae7bd4b7384bf000e687dfc
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-07 10:29:48 +0200

    (refactor): move ShowCalendar from ChooseBooking

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import { ChooseBooking } from "../ChooseBooking";
 import { CompleteBooking } from "../CompleteBooking";
-import { ColumnWrapper } from "../styled/Wrappers";
+import { FindBooking } from "../FindBooking";
+import { ShowCalendar } from "../ShowCalendar";
+import { CalendarWrapper, ColumnWrapper } from "../styled/Wrappers";
 
 export const Booking = () => {
   const timeToFillOutForm: boolean = true; //satt på true nu för att kunna visa komponent tills vidare...
+  const [newBooking, setNewBooking] = useState(false);
+  const createNewBooking = (value: boolean) => {
+    setNewBooking(value);
+  };
 
   return (
     <>
       <ColumnWrapper>
-        <ChooseBooking></ChooseBooking>
+        <ChooseBooking createNewBooking={createNewBooking}></ChooseBooking>
+        <CalendarWrapper>
+          {newBooking && <ShowCalendar></ShowCalendar>}
+        </CalendarWrapper>
+        {newBooking && <FindBooking></FindBooking>}
         {timeToFillOutForm && <CompleteBooking></CompleteBooking>}
       </ColumnWrapper>
     </>
