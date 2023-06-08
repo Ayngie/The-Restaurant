069e96c354bc0c57295a577cf7d9d1203de6186a
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-08 16:24:56 +0200

    (feat): create function to post booking to API

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -3,20 +3,56 @@ import { ChooseBooking } from "../ChooseBooking";
 import { CompleteBooking } from "../createNewBooking/CompleteBooking";
 import { ColumnWrapper } from "../styled/Wrappers";
 import { SearchUnbookedTimes } from "../createNewBooking/SearchUnbookedTimes";
+import { IBooking, defaultBooking } from "../../models/IBooking";
+import { IGuest } from "../../models/IGuest";
+import { createBooking } from "../../services/bookingService";
+import { NormalButton } from "../styled/StyledButtons";
 
 export const Booking = () => {
-  const timeToFillOutForm: boolean = true; //satt på true nu för att kunna visa komponent tills vidare...
+  const [booking, setBooking] = useState<IBooking>(defaultBooking);
+  const [timeToFillOutForm, SetTimeToFillOutForm] = useState(true);
   const [clickedNewBooking, setClickedNewBooking] = useState(false);
-  const createNewBooking = (value: boolean) => {
-    setClickedNewBooking(value);
+  const createNewBooking = (showForm: boolean) => {
+    setClickedNewBooking(showForm);
+  };
+  const dateForBooking = (details: IBooking) => {
+    console.log("details to send: ", details);
+    setBooking({
+      ...booking,
+      numberOfGuests: details.numberOfGuests,
+      date: details.date,
+      time: details.time,
+    });
+  };
+  const guestForDate = (guest: IGuest) => {
+    setBooking({
+      ...booking,
+      guest: {
+        ...booking.guest,
+        name: guest.name,
+        email: guest.email,
+        phoneNumber: guest.phoneNumber,
+      },
+    });
+  };
+
+  const postBooking = async () => {
+    await createBooking(booking);
   };
 
   return (
     <>
       <ColumnWrapper>
         <ChooseBooking createNewBooking={createNewBooking}></ChooseBooking>
-        {clickedNewBooking && <SearchUnbookedTimes></SearchUnbookedTimes>}
-        {timeToFillOutForm && <CompleteBooking></CompleteBooking>}
+        {clickedNewBooking && (
+          <SearchUnbookedTimes sendDate={dateForBooking}></SearchUnbookedTimes>
+        )}
+        {timeToFillOutForm && (
+          <>
+            <CompleteBooking sendBooking={guestForDate}></CompleteBooking>
+            <NormalButton onClick={postBooking}>Boka</NormalButton>
+          </>
+        )}
       </ColumnWrapper>
     </>
   );
