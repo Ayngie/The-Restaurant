ea3ff6d2690bd52cf6a51bd8dd81581df7b3db67
Per Berge <per.berge93@gmail.com>
2023-06-09 12:18:27 +0200

    (feat): added context, dispatch.

--- fed22s-therestaurant/src/components/pages/Admin.tsx
+++ fed22s-therestaurant/src/components/pages/Admin.tsx
@@ -1,3 +1,18 @@
+import { useReducer } from "react";
+import { AdminContext } from "../../contexts/AdminContext";
+import { AdminDispatchContext } from "../../contexts/AdminDispatchContext";
+import { AdminReducer } from "../../reducers/AdminReducer";
+import { AdminSearchBooking } from "../admin/AdminSearchBooking";
+
 export const Admin = () => {
-  return <>Admin!!!</>;
+  const [bookings, dispatch] = useReducer(AdminReducer, []);
+  return (
+    <>
+      <AdminContext.Provider value={bookings}>
+        <AdminDispatchContext.Provider value={dispatch}>
+          <AdminSearchBooking></AdminSearchBooking>
+        </AdminDispatchContext.Provider>
+      </AdminContext.Provider>
+    </>
+  );
 };
