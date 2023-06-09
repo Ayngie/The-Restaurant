caff01a71a93ccf8c7ae3fca010965de7d47649b
Ayngie <angelicareutersward@gmail.com>
2023-06-09 12:11:00 +0200

    ✨ (feat): added conditiona. component for search booking and added conditional loader
    Co-authored-by: Lisa Lindblom <lisalindblom@users.noreply.github.com>
    Co-authored-by: Per Berge <bergeper@users.noreply.github.com>

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -8,15 +8,24 @@ import { IGuest } from "../../models/IGuest";
 import { createBooking } from "../../services/bookingService";
 import { NormalButton, WarningButton } from "../styled/StyledButtons";
 import { Loader } from "../styled/Loader";
+import { HandleBooking } from "../showSingleBooking/HandleBooking";
 
 export const Booking = () => {
   const [booking, setBooking] = useState<IBooking>(defaultBooking);
   const [timeToFillOutForm, SetTimeToFillOutForm] = useState(false);
   const [clickedNewBooking, setClickedNewBooking] = useState(false);
+  const [clickedHandleBooking, setClickedHandleBooking] = useState(false);
   const [bookingSubmitted, setBookingSubmitted] = useState(false);
+  const [showLoader, setShowLoader] = useState(false);
 
   const createNewBooking = (showForm: boolean) => {
     setClickedNewBooking(showForm);
+    setClickedHandleBooking(false);
+  };
+
+  const handleBooking = (showSearchField: boolean) => {
+    setClickedHandleBooking(showSearchField);
+    setClickedNewBooking(false);
   };
 
   const dateForBooking = (details: IBooking) => {
@@ -58,13 +67,17 @@ export const Booking = () => {
   return (
     <>
       <ColumnWrapper>
-        <Loader></Loader>
-        <ChooseBooking createNewBooking={createNewBooking}></ChooseBooking>
+        <ChooseBooking
+          createNewBooking={createNewBooking}
+          handleBooking={handleBooking}></ChooseBooking>
+        {clickedHandleBooking && <HandleBooking></HandleBooking>}
         {clickedNewBooking && (
           <SearchUnbookedTimes
             sendDate={dateForBooking}
-            showForm={SetTimeToFillOutForm}></SearchUnbookedTimes>
+            showForm={SetTimeToFillOutForm}
+            showLoader={setShowLoader}></SearchUnbookedTimes>
         )}
+        {showLoader && <Loader></Loader>}
         {timeToFillOutForm && (
           <>
             <CompleteBooking
