8aa1d037d03944a25711980e43d746884e04077b
Per Berge <per.berge93@gmail.com>
2023-06-09 10:50:28 +0200

    (feat): dispatch-context for admin

---
+++ fed22s-therestaurant/src/contexts/AdminDispatchContext.ts
@@ -0,0 +1,6 @@
+import { Dispatch, createContext } from "react";
+import { IAction } from "../reducers/AdminReducer";
+
+export const AdminDispatchContext = createContext<Dispatch<IAction>>(() => {
+  return;
+});
