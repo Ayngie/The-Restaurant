8cb9aa285873d35f5a04666d58f83368ebd7e628
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-12 22:23:08 +0200

    (feat): create styled component for header

--- fed22s-therestaurant/src/components/layout/Layout.tsx
+++ fed22s-therestaurant/src/components/layout/Layout.tsx
@@ -2,11 +2,14 @@ import { Outlet } from "react-router";
 import { NavBar } from "./NavBar";
 import { Footer } from "./Footer";
 import { MainWrapper } from "../styled/Wrappers";
+import { StyledHeader } from "../styled/header/StyledHeader";
 
 export const Layout = () => {
   return (
     <>
-      <NavBar></NavBar>
+      <StyledHeader>
+        <NavBar></NavBar>
+      </StyledHeader>
       <MainWrapper>
         <Outlet></Outlet>
       </MainWrapper>
