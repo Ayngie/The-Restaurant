e5f76b41b67e60247d9e8b2eab34c3b8ae9bc791
Ayngie <angelicareutersward@gmail.com>
2023-06-12 22:36:22 +0200

    ✨ (feat): recieved props via interface, interpolated data in html, updated state of boolean (for rendering component in parent)

--- fed22s-therestaurant/src/components/admin/ShowSingleBooking.tsx
+++ fed22s-therestaurant/src/components/admin/ShowSingleBooking.tsx
@@ -1,29 +1,52 @@
 import { useContext } from "react";
+import { AdminDispatchContext } from "../../contexts/AdminDispatchContext";
 import { NormalButton, WarningButton } from "../styled/StyledButtons";
 import { ColumnWrapper, RowWrapper } from "../styled/Wrappers";
-import { AdminContext } from "../../contexts/AdminContext";
+import { IBooking } from "../../models/IBooking";
+// import { ActionType } from "../../reducers/AdminReducer";
 
-export const ShowSingleBooking = () => {
-  const bookings = useContext(AdminContext);
+interface IShowSingleBooking {
+  singleBooking?: IBooking;
+  showUpdateForm(show: boolean): void;
+}
 
-  //update booking
+export const ShowSingleBooking = ({
+  singleBooking,
+  showUpdateForm,
+}: IShowSingleBooking) => {
+  const dispatch = useContext(AdminDispatchContext);
+
   const updateBooking = () => {
+    showUpdateForm(true);
     console.log("Update");
   };
   //delete booking
   const deleteBooking = () => {
+    // dispatch({ type: ActionType.REMOVEBOOKING, payload: singleBooking._id });
     console.log("Delete");
   };
 
   //Avbryt
   const handleCancel = () => {
+    //skapa lifting state up till admin som döljer AdminHandleBooking komponenten?
     console.log("Avbryt");
   };
 
   return (
     <>
       <ColumnWrapper>
-        <h3> Show single booking</h3>
+        <ColumnWrapper>
+          <div>
+            <h3> Bokning:</h3>
+            <p>Namn: {singleBooking?.guest.name}</p>
+            <p>Epost: {singleBooking?.guest.email}</p>
+            <p>Tel: {singleBooking?.guest.phoneNumber}</p>
+            <p>Datum: {singleBooking?.date}</p>
+            <p>Tid: {singleBooking?.time}</p>
+            <p>Antal gäster: {singleBooking?.numberOfGuests}</p>
+            <p>Bokningsid: {singleBooking?._id}</p>
+          </div>
+        </ColumnWrapper>
         <RowWrapper>
           <NormalButton type="button" onClick={updateBooking}>
             Uppdatera bokning
