75ce2b0e8f2ceb9b2561ed6ac83c2736a647b851
Per Berge <per.berge93@gmail.com>
2023-06-12 12:31:54 +0200

    (feat): added outlet for context and router
    Co-Authored-By: Lisa Lindblom <113437951+lisalindblom@users.noreply.github.com>
    Co-Authored-By: Angie <71646930+Ayngie@users.noreply.github.com>

--- fed22s-therestaurant/src/components/pages/Admin.tsx
+++ fed22s-therestaurant/src/components/pages/Admin.tsx
@@ -1,16 +1,21 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { AdminContext } from "../../contexts/AdminContext";
 import { AdminDispatchContext } from "../../contexts/AdminDispatchContext";
 import { AdminReducer } from "../../reducers/AdminReducer";
-import { AdminSearchBooking } from "../admin/AdminSearchBooking";
+
+import { Outlet } from "react-router";
 
 export const Admin = () => {
   const [bookings, dispatch] = useReducer(AdminReducer, []);
+
+  useEffect(() => {
+    // Hämta alla bokningar här.
+  }, []);
   return (
     <>
       <AdminContext.Provider value={bookings}>
         <AdminDispatchContext.Provider value={dispatch}>
-          <AdminSearchBooking></AdminSearchBooking>
+          <Outlet></Outlet>
         </AdminDispatchContext.Provider>
       </AdminContext.Provider>
     </>
