8eac3d612ec9af1027139d9694ad77a7a052de75
Per Berge <per.berge93@gmail.com>
2023-06-12 12:38:21 +0200

    (feat): case added to get bookings by context or api
    Co-Authored-By: Lisa Lindblom <113437951+lisalindblom@users.noreply.github.com>
    Co-Authored-By: Angie <71646930+Ayngie@users.noreply.github.com>

--- fed22s-therestaurant/src/reducers/AdminReducer.ts
+++ fed22s-therestaurant/src/reducers/AdminReducer.ts
@@ -6,18 +6,19 @@ export interface IAction {
 }
 
 export enum ActionType {
-  GETBOOKINGS,
-  GETBOOKINGBYID,
+  GETBOOKINGSBYDATE,
+  GETBOOKINGBYIDWITHAPI,
+  GETBOOKINGBYIDFROMCONTEXT,
   UPDATEBOOKING,
   REMOVEBOOKING,
 }
 
 export const AdminReducer = (bookings: IBooking[], action: IAction) => {
   switch (action.type) {
-    case ActionType.GETBOOKINGS: {
+    case ActionType.GETBOOKINGSBYDATE: {
       return JSON.parse(action.payload);
     }
-    case ActionType.GETBOOKINGBYID: {
+    case ActionType.GETBOOKINGBYIDFROMCONTEXT: {
       return bookings.map((booking) => {
         if (booking._id === action.payload) {
           return booking;
@@ -25,6 +26,9 @@ export const AdminReducer = (bookings: IBooking[], action: IAction) => {
           return;
         }
       });
+    }
+    case ActionType.GETBOOKINGBYIDWITHAPI: {
+      return JSON.parse(action.payload);
     }
     case ActionType.UPDATEBOOKING: {
       return bookings;
