ac931ba66372129ba6cbe0b88b2f3fb2e20958ec
Per Berge <per.berge93@gmail.com>
2023-06-13 14:02:11 +0200

    (feat): added route

--- fed22s-backend/src/routes/api/adminRoutes.js
+++ fed22s-backend/src/routes/api/adminRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const {
   getAllBookings,
+  getBookingById,
   updateBookingById,
   deleteBookingById,
 } = require("../../controllers/api/adminControllers");
 
 router.get("/", getAllBookings);
+router.get("/:bookingId", getBookingById);
 router.put("/:bookingId", updateBookingById);
 router.delete("/:bookingId", deleteBookingById);
 
