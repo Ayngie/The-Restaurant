5f4216bb6017a279b6acf2618547103e27731e2c
Per Berge <per.berge93@gmail.com>
2023-06-13 12:57:19 +0200

    (refactor): fixed undefiend.

--- fed22s-therestaurant/src/components/pages/AdminHandleBooking.tsx
+++ fed22s-therestaurant/src/components/pages/AdminHandleBooking.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router";
 import { AdminContext } from "../../contexts/AdminContext";
 import { useContext, useEffect, useState } from "react";
-import { IBooking } from "../../models/IBooking";
+import { IBooking, defaultBooking } from "../../models/IBooking";
 import { ShowSingleBooking } from "../admin/ShowSingleBooking";
 import { ColumnWrapper } from "../styled/Wrappers";
 import { UpdateBooking } from "../showSingleBooking/UpdateBooking";
@@ -9,20 +9,19 @@ import { UpdateBooking } from "../showSingleBooking/UpdateBooking";
 export const AdminHandleBooking = () => {
   const { id } = useParams();
   const bookings = useContext(AdminContext);
-  const [booking, setBooking] = useState<IBooking>();
+  const [booking, setBooking] = useState<IBooking>(defaultBooking);
   const [showUpdateForm, setShowUpdateForm] = useState(false);
   const [bookingIsDeleted, setBookingIsDeleted] = useState(false);
 
   //mockdata
   useEffect(() => {
-    if (booking) return;
-
-    const findBooking = bookings.find((booking) => booking._id === id);
-    if (findBooking) {
-      setBooking(findBooking);
+    // if (booking) return;
+    const findBooking = bookings.filter((booking) => booking._id === id);
+    if (findBooking.length === 1) {
+      setBooking(findBooking[0]);
     }
     console.log(findBooking);
-  }, [booking, id]);
+  }, [bookings, booking, id]);
 
   return (
     <>
