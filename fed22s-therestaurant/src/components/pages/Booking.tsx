cb1d8430dcdb2102b2f676eecbe0c70fd33fb86a
Per Berge <per.berge93@gmail.com>
2023-06-14 16:15:00 +0200

    (feat): sets state when cancelbuttons called

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -6,18 +6,24 @@ import { SearchUnbookedTimes } from "../createNewBooking/SearchUnbookedTimes";
 import { IBooking, defaultBooking } from "../../models/IBooking";
 import { IGuest } from "../../models/IGuest";
 import { createBooking } from "../../services/bookingService";
-import { NormalButton, WarningButton } from "../styled/StyledButtons";
 import { Loader } from "../styled/Loader";
 import { HandleBooking } from "../showSingleBooking/HandleBooking";
 
 export const Booking = () => {
   const [booking, setBooking] = useState<IBooking>(defaultBooking);
-  const [timeToFillOutForm, SetTimeToFillOutForm] = useState(false);
+  const [timeToFillOutForm, setTimeToFillOutForm] = useState(false);
   const [clickedNewBooking, setClickedNewBooking] = useState(false);
   const [clickedHandleBooking, setClickedHandleBooking] = useState(false);
   const [bookingSubmitted, setBookingSubmitted] = useState(false);
   const [showLoader, setShowLoader] = useState(false);
   const [showOptions, setShowOptions] = useState(true);
+
+  const cancelSearch = () => {
+    setShowOptions(true);
+    setClickedNewBooking(false);
+    setClickedHandleBooking(false);
+    setTimeToFillOutForm(false);
+  };
 
   const createNewBooking = (showForm: boolean) => {
     setClickedNewBooking(showForm);
@@ -28,7 +34,7 @@ export const Booking = () => {
   const handleBooking = (showSearchField: boolean) => {
     setClickedHandleBooking(showSearchField);
     setClickedNewBooking(false);
-    SetTimeToFillOutForm(false);
+    setTimeToFillOutForm(false);
     setShowOptions(false);
   };
 
@@ -71,7 +77,7 @@ export const Booking = () => {
     if (bookingSubmitted) {
       setClickedNewBooking(false);
 
-      SetTimeToFillOutForm(false);
+      setTimeToFillOutForm(false);
     }
   }, [bookingSubmitted]);
 
@@ -81,16 +87,20 @@ export const Booking = () => {
         {showOptions && (
           <ChooseBooking
             createNewBooking={createNewBooking}
-            handleBooking={handleBooking}></ChooseBooking>
+            handleBooking={handleBooking}
+          ></ChooseBooking>
         )}
 
-        {clickedHandleBooking && <HandleBooking></HandleBooking>}
+        {clickedHandleBooking && (
+          <HandleBooking cancel={cancelSearch}></HandleBooking>
+        )}
 
         {clickedNewBooking && !timeToFillOutForm && (
           <SearchUnbookedTimes
             sendDate={dateForBooking}
-            showForm={SetTimeToFillOutForm}
-            showLoader={setShowLoader}></SearchUnbookedTimes>
+            showForm={setTimeToFillOutForm}
+            showLoader={setShowLoader}
+          ></SearchUnbookedTimes>
         )}
         {showLoader && <Loader></Loader>}
         {timeToFillOutForm && (
@@ -103,7 +113,8 @@ export const Booking = () => {
 
             <CompleteBooking
               sendBooking={guestForDate}
-              postBooking={postBooking}></CompleteBooking>
+              postBooking={postBooking}
+            ></CompleteBooking>
           </>
         )}
         {bookingSubmitted && (
