8249f2dddc5c4f3599ae1bb8b18890d806588c04
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-14 11:15:58 +0200

    (style): add hero to startpage

---
+++ fed22s-therestaurant/src/components/styled/StyledHero.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledHero = styled.section`
+  background-image: url(https://images.pexels.com/photos/1549198/pexels-photo-1549198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 90vh;
+`;
