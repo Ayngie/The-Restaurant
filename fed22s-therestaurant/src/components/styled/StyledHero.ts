04c59470a80163fb32d0ef83d3da26c1ce6635db
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-14 11:53:29 +0200

    (refactor): move flexdirection

--- fed22s-therestaurant/src/components/styled/StyledHero.ts
+++ fed22s-therestaurant/src/components/styled/StyledHero.ts
@@ -6,4 +6,9 @@ export const StyledHero = styled.section`
   background-size: cover;
   width: 100%;
   height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
 `;
--- fed22s-therestaurant/src/components/styled/StyledSection.ts
+++ fed22s-therestaurant/src/components/styled/StyledSection.ts
@@ -2,4 +2,9 @@ import styled from "styled-components";
 
 export const StyledSection = styled.section`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 50vh;
 `;
