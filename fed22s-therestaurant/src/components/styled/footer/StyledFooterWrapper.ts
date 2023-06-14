f473bdf09d8d323861067ed1f20368f84d84c690
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-06-14 11:14:24 +0200

    (style): make footer responsive and update info

--- fed22s-therestaurant/src/components/styled/footer/StyledFooterWrapper.ts
+++ fed22s-therestaurant/src/components/styled/footer/StyledFooterWrapper.ts
@@ -3,10 +3,12 @@ import styled from "styled-components";
 export const StyledFooterWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-  background-color: black;
+  background-color: #9962af;
   color: antiquewhite;
+  padding: 60px 0px 60px 60px;
   @media screen and (min-width: 1200px) {
     flex-direction: row;
+    justify-content: space-around;
+    padding: 60px 20px 60px 20px;
   }
 `;
