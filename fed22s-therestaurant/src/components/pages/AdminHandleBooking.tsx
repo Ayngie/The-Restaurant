1deef06072cfbcad23f1de369fa59aff63a80687
Per Berge <per.berge93@gmail.com>
2023-06-14 12:56:19 +0200

    (refactor): hides booking and calculates how many free tables

--- fed22s-therestaurant/src/components/pages/AdminHandleBooking.tsx
+++ fed22s-therestaurant/src/components/pages/AdminHandleBooking.tsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router";
 import { AdminContext } from "../../contexts/AdminContext";
-import { useContext, useEffect, useState } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import { IBooking, defaultBooking } from "../../models/IBooking";
 import { ShowSingleBooking } from "../admin/ShowSingleBooking";
 import { ColumnWrapper } from "../styled/Wrappers";
 import { UpdateBooking } from "../showSingleBooking/UpdateBooking";
+import { checkBookedTables } from "../../helpers/checkBookedTables";
 
 export const AdminHandleBooking = () => {
   const { id } = useParams();
@@ -12,11 +13,17 @@ export const AdminHandleBooking = () => {
   const [booking, setBooking] = useState<IBooking>(defaultBooking);
   const [showUpdateForm, setShowUpdateForm] = useState(false);
   const [bookingIsDeleted, setBookingIsDeleted] = useState(false);
+  const [freeTables, setFreeTables] = useState(0);
 
-  console.log("bookings: ", bookings);
-
-  //mockdata
   useEffect(() => {
+    if (booking.numberOfGuests > 0) {
+      const tables = checkBookedTables(
+        bookings,
+        booking.numberOfGuests,
+        booking.time
+      );
+      setFreeTables(15 - tables);
+    }
     if (booking.numberOfGuests > 0) return;
 
     const findBooking = bookings.filter((booking) => booking._id === id);
@@ -28,7 +35,7 @@ export const AdminHandleBooking = () => {
   return (
     <>
       <ColumnWrapper>
-        {!bookingIsDeleted && (
+        {!bookingIsDeleted && !showUpdateForm && (
           <ShowSingleBooking
             singleBooking={booking}
             showUpdateForm={setShowUpdateForm}
@@ -36,7 +43,15 @@ export const AdminHandleBooking = () => {
           ></ShowSingleBooking>
         )}
         {bookingIsDeleted && <h3>Bokningen är raderad</h3>}
-        {showUpdateForm && <UpdateBooking booking={booking}></UpdateBooking>}
+        {showUpdateForm && (
+          <>
+            <h3>Lediga bord: {freeTables}</h3>
+            <UpdateBooking
+              guestAbleToBook={freeTables}
+              booking={booking}
+            ></UpdateBooking>
+          </>
+        )}
       </ColumnWrapper>
     </>
   );
