0d9e20bc474ce10b36fe5c7711e2575fef70e854
Ayngie <angelicareutersward@gmail.com>
2023-06-15 09:13:11 +0200

    ✨ (feat): added course description, and styled components / wrapper

--- fed22s-therestaurant/src/components/pages/Menu.tsx
+++ fed22s-therestaurant/src/components/pages/Menu.tsx
@@ -3,10 +3,10 @@ import { NormalButton } from "../styled/StyledButtons";
 import {
   StyledH1,
   StyledMenuParagraph,
-  StyledParagraph,
+  StyledMenuListItem,
+  StyledCourseDescription,
 } from "../styled/TextStyles";
-import { ColumnWrapper } from "../styled/Wrappers";
-import { StyledListItem } from "../styled/menu/StyledListItem";
+import { ColumnWrapper, MenuWrapper } from "../styled/Wrappers";
 import { useEffect } from "react";
 
 export const Menu = () => {
@@ -26,13 +26,32 @@ export const Menu = () => {
     "Smältande chokladtårta med saltkaramellglass och hallonkompott",
     "Lätt citronpannacotta med färska bär och myntaströssel",
   ];
+  const courseDescription: string[] = [
+    "En vacker tallrik med finhackad färskrökt lax blandad med en krämig avokado- och limecrème, serverad med fina garneringar av örter och krispiga brödkrutonger.",
+    "Perfekt grillade pilgrimsmusslor som serveras över en krämig parmesanrisotto, toppad med en delikat balsamicoreduktion och dekorativa örter.",
+    "En saftig och perfekt grillad entrecôte som serveras med en rik och smakfull rödvinssås, tillsammans med krispigt rostad vitlökspotatis och färska grönsaker.",
+    "En al dente kokt pasta med en krämig tryffelsås, sauterade champinjoner och generöst strödd med färskriven parmesan.",
+    "Saftigt citronmarinerat kycklingbröst serverat på en säng av kryddig couscous med färska örter, tillsammans med knaprig grön sparris.",
+    "En färgstark rödbetsbiff med en härlig kryddning, serverad med syrlig picklad rödlök och en krämig getostkräm.",
+    "En vacker havsabborrefilé som är smörstekt till perfektion och serverad med en frisk tomat- och kaprisrelish som kompletterar den milda smaken av fisken.",
+    "Hemgjord vegetarisk ravioli fylld med en krämig blandning av ricotta och spenat, serverad med en läcker salvia- och hasselnötssmörssås.",
+    "En lockande chokladtårta med en fyllning av smältande choklad, serverad med en läcker saltkaramellglass och en syrlig hallonkompott.",
+    "En lätt och krämig citronpannacotta med en uppfriskande citrusarom, serverad med färska och färgstarka bär som smakar sommar. Toppad med ett lätt och fräscht myntaströssel som ger en extra touch av fräschör till denna delikata dessert.",
+  ];
 
   const html = (
-    <ol>
-      {menuList.map((course, index) => (
-        <StyledListItem key={index}>{course}</StyledListItem>
-      ))}
-    </ol>
+    <MenuWrapper>
+      <ol>
+        {menuList.map((course, index) => (
+          <StyledMenuListItem key={index}>
+            {course}
+            <StyledCourseDescription>
+              {courseDescription[index]}
+            </StyledCourseDescription>
+          </StyledMenuListItem>
+        ))}
+      </ol>
+    </MenuWrapper>
   );
   return (
     <ColumnWrapper>
