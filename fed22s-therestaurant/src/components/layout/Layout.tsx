5f7808c1566ececaf9458e755eabac3e4f404324
Ayngie <angelicareutersward@gmail.com>
2023-06-27 10:46:31 +0200

    ♻️ (refactor): removed unnused imports in layout component

--- fed22s-therestaurant/src/components/layout/Layout.tsx
+++ fed22s-therestaurant/src/components/layout/Layout.tsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router";
-import { NavBar } from "./NavBar";
 import { Footer } from "./Footer";
 import { MainWrapper } from "../styled/Wrappers";
-import { StyledHeader } from "../styled/header/StyledHeader";
 import { Header } from "./Header";
 
 export const Layout = () => {
