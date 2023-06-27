49393643e82ce990bec1818a831911f1ea11fb4e
Ayngie <angelicareutersward@gmail.com>
2023-06-27 10:44:42 +0200

    ♻️ (refactor): removed window refresh

--- fed22s-therestaurant/src/components/layout/Header.tsx
+++ fed22s-therestaurant/src/components/layout/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
   return (
     <>
       <StyledHeader>
-        <Link to="/" onClick={() => window.location.reload()}>
+        <Link to="/">
           <StyledLogo src={logo} alt="logga" />
         </Link>
         <NavBar></NavBar>
